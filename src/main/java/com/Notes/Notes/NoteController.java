53324f8f711ff18b4c0e1a4663cd1d9140ad2676
MyFirstSpringBoot Project <0112cs201019@gmail.com>
2025-09-04 18:29:47 +0530

    Add gitattributes to fix line endings

--- src/main/java/com/Notes/Notes/NoteController.java
+++ src/main/java/com/Notes/Notes/NoteController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/notes")
 @CrossOrigin(origins = "*") // allow frontend access from Vercel later
 public class NoteController {
 
